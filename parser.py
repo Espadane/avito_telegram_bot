a872146a4cbbf9fecbd34ca7ade7fa465798fa39
Espadane <52450374+Espadane@users.noreply.github.com>
2021-12-07 23:46:46 +0300

    Add files via upload

--- parser.py
+++ parser.py
@@ -34,9 +34,10 @@ def get_data_from_html(url):
             'upgrade-insecure-requests': '1',
             'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
         try:
-            r = requests.get(url=url, headers=header,proxies=proxy)
+            r = requests.get(url=url, headers=header,proxies=proxy, timeout=2)
         except Exception as ex:
             print(ex)
+            r = requests.get(url=url, headers=header, timeout=2)
         print(r.status_code)
         if r.status_code == 429:
             
@@ -45,8 +46,9 @@ def get_data_from_html(url):
             soup = BeautifulSoup(r.text, 'lxml')
             ads = soup.find_all('div', class_='iva-item-root-Nj_hb')
             return ads
-    except:
-        print(Exception)
+    except Exception as e:
+        print('!')
+        print(e)
 
 def get_ad_data(url):
     """Получение данных последнего объявления"""
@@ -65,18 +67,40 @@ def get_ad_data(url):
                 ad_data = [ad_title, ad_price, ad_content, ad_location, ad_url]
 
                 return ad_data
-        except:
-                print(Exception)
+        except Exception as e:
+            print('get_ad_data')
+            print(e)
+
 
 def get_requested_page_title(url):
     """Получение Title поискового запроса"""
     try:
-        headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'}
-        r = requests.get(url=url, headers=headers)
+        with open('proxies') as file:
+            proxy_base = ''.join(file.readlines()).strip().split('\n')
+            proxy_adr = random.choice(proxy_base)
+            proxy = {'https' : f'https://{proxy_login}:{proxy_pass}@{proxy_adr}',
+                    'http' : f'http://{proxy_login}:{proxy_pass}@{proxy_adr}'}
+        header = {
+            'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+            'accept-encoding':'gzip, deflate, br',
+            'accept-language':'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+            'cache-control':'no-cache',
+            'dnt': '1',
+            'pragma': 'no-cache',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
+        try:
+            r = requests.get(url=url, headers=header, proxies=proxy, timeout=2)
+        except:
+            r = requests.get(url=url, headers=header, timeout=2)
         soup = BeautifulSoup(r.text, 'lxml')
         page_title = soup.title.text
         page_title = page_title.split('|')[0]
 
         return page_title
-    except:
-        print(Exception)
+    except Exception as ex:
+        print(ex)
+        print('get_requested_page_title')
